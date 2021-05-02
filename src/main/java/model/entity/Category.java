09f5924948d0309c7dbcec75b6548d277de4d06f
이효곤 <LeeHyogon>
2021-05-02 17:35:10 +0900

    6장 카테고리 계층 구조

--- src/main/java/model/entity/Category.java
+++ src/main/java/model/entity/Category.java
@@ -19,7 +19,24 @@ public class Category {
     private String name;
 
     @OneToMany
-    private List<Item> items=new ArrayList<Item>();
+    private List<Item> items = new ArrayList<Item>();
 
+    //카테고리의 계층 구조를 위한 필드들
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child=new ArrayList<Category>();
+
+    //연관관계 메소드
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
 
 }
